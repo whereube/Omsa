13e5e9abf9de4a471c10342d7df6c61510c9657a
whereube <100147393+whereube@users.noreply.github.com>
2022-04-07 13:26:03 +0200

    uppdaterat profil.py

---
+++ profile.py
@@ -0,0 +1,43 @@
+
+import psycopg2 
+from datetime import date
+
+user_id = ""
+admin = False
+current_date = date.today()
+
+def open_db_online_store():
+    try:
+        connection = psycopg2.connect(
+            user = 'am6110',
+            password = 'y4dpegsc', 
+            host = 'pgserver.mau.se',
+            port = '5432',
+            database = 'am6110'
+            )
+        return connection
+    except: 
+        print("Databasen finns inte!")
+
+def close_db_online_store(connection):
+    connection.close()
+
+def login(user_email, user_password):
+    
+    connection = open_db_online_store()
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT email, password FROM \"profile\" where email = %s and password = %s", (user_email, user_password))
+    records = cursor.fetchall()
+    
+
+    if records == []:
+        print(records)
+        print(user_email)
+        print(user_password)
+    else:    
+        return 1
+
+    close_db_online_store(connection)
+                
+                
