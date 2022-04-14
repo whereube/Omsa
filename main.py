fd975818278bb03f2609336698f884bd7886e9b5
whereube <100147393+whereube@users.noreply.github.com>
2022-04-14 13:52:40 +0200

    version 1

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import os
 
 UPLOAD_FOLDER = 'static/article_images'
 
-app = Flask(__name__)
+
 app = Flask(__name__, static_url_path='/static')
 
 app.run(debug=True)
 app.config['SECRET_KEY'] = 'thisissecret'
-#if __name__ == '__main__':
-    #app.run(debug=True)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
+#if __name__ == '__main__':
+    #app.run(debug=True)
 
 
 @app.route("/create")
@@ -72,7 +72,8 @@ def view_articles():
 
 @app.route("/")
 def start():
-    return render_template("index.html")
+    user_id = session.get('USER_NAME')
+    return render_template("index.html", user_id=user_id)
 
     
 '''Login function'''
@@ -101,6 +102,7 @@ def logout():
     session.pop("USER_ID")
     session.pop("USER_NAME")
     return render_template("logged_out.html")
+
 
 
 @app.route("/show_profile_page")
