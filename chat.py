b3c9c607a65bb7df9c5fe82349f1a47184d9f3df
whereube <100147393+whereube@users.noreply.github.com>
2022-05-05 14:51:27 +0200

    vidare arbete med visa meddelande

--- chat.py
+++ chat.py
@@ -57,8 +57,10 @@ def get_chat_messages(chat_id):
     connection = open_db_omsa()
     cursor = connection.cursor()
     cursor.execute("""
-    select * from message 
+    select message.id, message.chat_id, message.user_id, message.text, message.time_sent, profile.user_name 
+    inner join profile on message.user_id = profile.id
     where chat_id = %s 
+    order by time_sent desc
     """,(chat_id,))
 
     records = cursor.fetchall()
@@ -74,15 +76,15 @@ def chat_message_to_db(user_message):
     get_chat_id_from_db = get_chat_id(user_id)
     for row in get_chat_id_from_db:
         chat_id = row[0]
-
+    time_now = datetime.now()
     message_id = uuid.uuid4()
     connection = open_db_omsa()
     
     cursor = connection.cursor()
     cursor.execute("""
-    insert into message (id, chat_id, user_id, text)
-    values (%s, %s, %s, %s)
-    """,(message_id, chat_id, user_id, user_message))
+    insert into message (id, chat_id, user_id, text, time_sent)
+    values (%s, %s, %s, %s, %s)
+    """,(message_id, chat_id, user_id, user_message, time_now))
 
     cursor.close()
     connection.commit()
